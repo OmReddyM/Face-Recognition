53b985f6b89f1205762b32d07e54f5ba6fc156b6
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2021-12-11 17:15:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-"https://teachablemachine.withgoogle.com/models/Dw4DlwC8J/"
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function Snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='capturedImage' src='" + data_uri + "'>"
+    });
+}
+
+console.log("ML5 version: ", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Dw4DlwC8J/modal.json", function () { console.log("Modal Loaded!"); });
